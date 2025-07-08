09569286c36cae87186181bb2689d61d9311d010
alireza <farmanialireza668@gmail.com>
2025-07-08 16:23:44 +0330

    set routes

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"os"
+	"telephone/internal/api"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -16,7 +17,11 @@ func CreateServer() {
 	})
 
 	// create routes for user
-	
-	
+	app.Get("/users", api.GetUsers)
+	app.Get("/users/:id", api.GetUserById)
+	app.Post("/users", api.CreateUser)
+	app.Put("/users/:id", api.UpdateUser)
+	app.Delete("/users/:id", api.DeleteUser)
+
 	app.Listen(port)
 }
